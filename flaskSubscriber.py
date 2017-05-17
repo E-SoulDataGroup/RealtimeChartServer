7f3c950bf1e42b0f5c39a6ad64b3fa4e34feadb8
root <root@hadoop1.test.yunwei.puppet.dh>
2017-05-17 11:33:23 +0800

    完善功能

--- flaskSubscriber.py
+++ flaskSubscriber.py
@@ -52,7 +52,7 @@ def test_message():
         if item['action_date'] != datetime.datetime.now().strftime('%Y-%m-%d'):
             continue
             
-        # 插入历史记录数据
+        # 插入历史记录数据,间隔超过几分钟的数据才处理
         if (current_time-previous_record_time).seconds >= chart_node_interval:
             if signal:
                 if item['ad_id'] in data:
@@ -61,11 +61,13 @@ def test_message():
                     data[item['ad_id']] = [[item['pv'], item['uv'], item['action_date'], item['log_time']]]
                 signal = False
             else:
+                # 同一批数据，全部积累起来
                 if (current_time - previous_time).seconds < 5:
                     if item['ad_id'] in data:
                         data[item['ad_id']].append([item['pv'], item['uv'], item['action_date'], item['log_time']])
                     else:
                         data[item['ad_id']] = [[item['pv'], item['uv'], item['action_date'], item['log_time']]]        
+                # 不同批数据，更新时间戳等待下次间隔
                 else:
                     previous_record_time = previous_time
                     signal = True
@@ -80,7 +82,7 @@ def test_message():
             #if update_chart: # 如果true，socket每分钟发送最新记录，否则不发送等待init事件
             for user,ad in user_ad_state.iteritems():
                 data_send = realtime_data_filter(data_current, ad)
-                socketio.emit('my response', data_send, room=user+'update', namespace='/test')
+                socketio.emit('my response', data_send, room=str(user)+'update', namespace='/test')
             data_current = {}
         else:
             continue
@@ -105,10 +107,15 @@ def handle_init_event(msg):
     print "************************"
     #update_chart = False
     join_room(msg['session_id'])
-    user_ad_state[msg['session_id']] = msg['ad_id']
+    # 用户没有选择进行init时先约定随机选取n个id进行展示
+    if '0' in ids or 0 in msg['ad_id']:
+        ad_ids = random.sample(data.keys(), len(msg['ad_id']))
+    else:
+        ad_ids = msg['ad_id']
+    user_ad_state[msg['session_id']] = ad_ids
     #if isinstance(msg, list):
     # 删除间隔两分钟内的历史记录
-    data_filtered = history_data_filter(data, msg['ad_id'])
+    data_filtered = history_data_filter(data, ad_ids)
     # socket发送历史记录，恢复原始设置
     socketio.emit('init', data_filtered, room=msg['session_id'], namespace='/test')
 
@@ -120,14 +127,18 @@ def handle_update_event(msg):
     #global update_chart
     # 开始每分钟socket最新记录，只取所需要的id
     #update_chart = True
-    join_room(msg['session_id'] + 'update')
+    join_room(str(msg['session_id']) + 'update')
 
 def history_data_filter(data_all, ids):
     global chart_node_interval
+    # 回传数据包含所有选择的id，不存在的id则为空列表
     data_cluster = {}
+    for key in ids:
+        data_cluster[key] = []
     today_zero = datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d') + " 00:00:00", '%Y-%m-%d %H:%M:%S')
     for key, value in data_all.iteritems():
         if key in ids:
+            # 补充当天0点到现在的历史数据，用0代替
             oldest_time = datetime.datetime.strptime(value[0][3], '%Y-%m-%d %H:%M:%S')
             while ((oldest_time - datetime.timedelta(seconds=chart_node_interval)) >= today_zero):
                 value.insert(0, [0, 0, datetime.datetime.now().strftime('%Y-%m-%d'), (oldest_time - datetime.timedelta(seconds=chart_node_interval)).strftime('%Y-%m-%d %H:%M:%S')])
@@ -137,6 +148,8 @@ def history_data_filter(data_all, ids):
 
 def realtime_data_filter(data_realtime, ids):
     data_filtered = {}
+    for key in ids:
+        data_filtered[key] = []
     for key, value in data_realtime.iteritems():
         if key in ids:
             data_filtered[key] = value
